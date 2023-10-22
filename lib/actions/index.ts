41339b0ce4d0150b6d9e2e103d6d7c741f270ad8
kartikgupta <kartik.gupta2505@gmail.com>
2023-10-22 18:05:11 +0530

    defining product model

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,12 +1,30 @@
 "use server"
 
+import Product from "../models/product.model";
 import { scrapeAmazonProduct } from "../scraper";
+import { connectToDB } from "../scraper/mongoose";
 
 export async function scrapeAndStoreProduct(productUrl: string) {
     if (!productUrl) return;
 
     try {
+        connectToDB();
         const scrapedProduct = await scrapeAmazonProduct(productUrl);
+
+        if(!scrapedProduct) return;
+
+        let product = scrapedProduct;
+        
+        const existingProduct = await Product.findOne({ url : scrapedProduct.url});
+
+        if(existingProduct){
+            const updatedPriceHistory = [
+                ...existingProduct.priceHistory,
+                { price : scrapedProduct.currentPrice}
+            ]
+        }
+
+
     } catch (error: any) {
         throw new Error(`Failed to create/update product: ${error.message}`);
     }
--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -62,10 +62,12 @@ export async function scrapeAmazonProduct(url: string) {
             reviewsCount : 100,
             stars : 4.5,
             isOutOfStock : outOfStock,
-            description
+            description,
+            lowestPrice : Number(currentPrice) || Number(originalPrice),
+            highestPrice : Number(originalPrice) || Number(currentPrice),
+            averagePrice : Number(currentPrice) || Number(originalPrice)
         }
-        console.log(data);
-
+        return data;
     } catch (error: any) {
         throw new Error(`Failed to scrape product: ${error.message}`);
     }
